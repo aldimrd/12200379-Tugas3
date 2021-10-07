bcc0fd24d7f61cd847d3f93fb293513f0474299d
aldimrd <amardian532@gmail.com>
2021-10-07 09:25:13 +0700

    menambahkan, modifikasi, edit_data.php

--- application/models/M_data.php
+++ application/models/M_data.php
@@ -9,7 +9,19 @@ class M_data extends CI_Model{
     function tampil_data(){
         return $this->db->get('mahasiswa');
     }
-}
 
-?>
- 
+    function edit_data($where,$table){
+        return $this->db->get_where($table,$where);
+    }
+
+    function update_data($where,$data,$table){
+        return $this->db->where($where);
+        return $this->db->update($table,$data);
+    }
+    
+    function hapus_data($where,$table){
+        $this->db->where($where);
+        $this->db->delete($table);
+
+    }
+    ?>
